eb86438d8a46b57935765a4a20602322b3991235
mbikal <mbikal85@gmail.com>
2026-04-29 01:07:55 +0545

    stars

--- src/main.tsx
+++ src/main.tsx
@@ -7,5 +7,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/*<App />*/}
     <StarRating maxRating={5}/>
+    <StarRating maxRating={5} size = {24} color = "red" />
   </StrictMode>,
 )
