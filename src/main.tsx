ec304d307552a8f450d6d7786782b23d72780789
mbikal <mbikal85@gmail.com>
2026-04-29 01:18:50 +0545

    default rating

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import StarRating from './StarRating'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/*<App />*/}
-    <StarRating maxRating={5}/>
-    <StarRating maxRating={5} size = {24} color = "red" />
+    <StarRating maxRating={5} defaultRating={3}/>
   </StrictMode>,
 )
